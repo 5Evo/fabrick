4ff52cee5b1017904a8fdd6d2790c8e51bc2b95d
alex <aleksei.petaikin@gmail.com>
2022-12-02 19:03:09 +0500

    final

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ To run the project, follow the steps:
 
     Clone the project to a local machine
     install dependencies from the file requirements.txt
-    Open the terminal and go to the /CRM_project 48/project48 directory
+    Open the terminal and go to the /fabrick/fabrick directory
     execute the commands:
 
     python manage.py makemigrations
--- mailing/models.py
+++ mailing/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
+
 # Create your models here.
 
 
@@ -43,13 +44,44 @@ class Mailing(models.Model):
     """
     objects = models.Manager()
 
-    start_time = models.DateTimeField()     # время запуска рассылки
-    stop_time = models.DateTimeField()      # Время окончания рассылки
-    text_message = models.TextField()            # Сообщение
-    filter_mobile = models.ForeignKey(MobileCode, null=True, on_delete=models.CASCADE)                       # фильтр по мобильному оператору получателя
-    filter_tag = models.ForeignKey(Tag, null=True, on_delete=models.CASCADE)       # фильтр по тэгу
-    create = models.DateTimeField(auto_now_add=True)        # время cоздания рассылки (для статисики и поиска)
-    update = models.DateTimeField(auto_now=True)            # время изменения рассылки (для статисики и поиска)
+    start_time = models.DateTimeField()  # время запуска рассылки
+    stop_time = models.DateTimeField()  # Время окончания рассылки
+    text_message = models.TextField()  # Сообщение
+    filter_mobile = models.ForeignKey(MobileCode, null=True,
+                                      on_delete=models.CASCADE)  # фильтр по мобильному оператору получателя
+    filter_tag = models.ForeignKey(Tag, null=True, on_delete=models.CASCADE)  # фильтр по тэгу
+    create = models.DateTimeField(auto_now_add=True)  # время cоздания рассылки (для статисики и поиска)
+    update = models.DateTimeField(auto_now=True)  # время изменения рассылки (для статисики и поиска)
+
+    def save(self, *args, **kwargs):
+        """
+        Переопределим метод save, чтобы при сохранении рассылки мы могли ее запустить или поставить в очередь
+        """
+        import datetime
+        now = datetime.datetime.now()
+        if self.start_time.timestamp() < now.timestamp() < self.stop_time.timestamp():
+            self.send_message(now)
+        else:
+            self.put_in_queue(now)
+        super(Mailing, self).save(*args, **kwargs)
+
+    def send_message(self, now):
+        """
+        TODO: Запускаем рассылку
+        :param now: текущее время
+        :return:
+        """
+        print(f'{self.id} start: {self.start_time}, now: {now}, stop: {self.stop_time}')
+        pass
+
+    def put_in_queue(self, now):
+        """
+        TODO: Постановка рассылки в очередь
+        :param now: текущее время
+        :return:
+        """
+        print(f'{self.id} NOT NOW {now}')
+        pass
 
 
 class Client(models.Model):
@@ -62,7 +94,7 @@ class Client(models.Model):
     objects = models.Manager()
     phone = models.PhoneNumberField()
     mobile_code = models.ForeignKey(MobileCode, on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Tag, blank=True)      # у каждого клиента может быть несколько Тэгов
+    tags = models.ManyToManyField(Tag, blank=True)  # у каждого клиента может быть несколько Тэгов
     timezone = models.CharField(max_length=32, choices=TIMEZONES, default='UTC')
 
 
@@ -72,7 +104,7 @@ class Message(models.Model):
     """
 
     objects = models.Manager()
-    create = models.DateTimeField(auto_now_add=True)        # время cоздания Сообщения
+    create = models.DateTimeField(auto_now_add=True)  # время cоздания Сообщения
     status = models.ForeignKey(Status, on_delete=models.CASCADE)
     id_mailing = models.ForeignKey(Mailing, on_delete=models.CASCADE)
     id_client = models.ForeignKey(Client, on_delete=models.CASCADE)
